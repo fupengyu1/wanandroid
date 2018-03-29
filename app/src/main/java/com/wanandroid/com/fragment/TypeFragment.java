0b2a5dcbe4d72d9ce525b418eb305cf9814d5384
fupengyu <fupengyu@qizidai.com>
2018-03-29 20:03:23 +0800

    qqqqqqqqq

--- app/src/main/java/com/wanandroid/com/fragment/TypeFragment.java
+++ app/src/main/java/com/wanandroid/com/fragment/TypeFragment.java
@@ -1,14 +1,25 @@
 package com.wanandroid.com.fragment;
 
+import android.graphics.Point;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 
 import com.gyf.barlibrary.ImmersionBar;
+import com.scwang.smartrefresh.header.WaveSwipeHeader;
+import com.scwang.smartrefresh.layout.api.RefreshLayout;
+import com.scwang.smartrefresh.layout.constant.SpinnerStyle;
+import com.scwang.smartrefresh.layout.footer.BallPulseFooter;
+import com.scwang.smartrefresh.layout.listener.OnRefreshListener;
 import com.wanandroid.com.R;
 import com.wanandroid.com.base.BaseFragment;
 import com.wanandroid.com.base.BasePresenter;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Timer;
+import java.util.TimerTask;
 
 import butterknife.Bind;
 
@@ -21,6 +32,7 @@ public class TypeFragment extends BaseFragment {
 
     @Bind(R.id.toolbar)
     Toolbar toolbar;
+    private RefreshLayout refreshLayout;
 
     @Override
     protected BasePresenter createPresenter() {
@@ -43,10 +55,128 @@ public class TypeFragment extends BaseFragment {
     }
 
     @Override
+    public void initView(View rootView) {
+        super.initView(rootView);
+        refreshLayout = (RefreshLayout) rootView.findViewById(R.id.refreshLayout);
+//        RefreshHeader refreshHeader = refreshLayout.getRefreshHeader();
+//        if (refreshHeader instanceof StoreHouseHeader) {
+//            ((StoreHouseHeader) refreshHeader).initWithPointList();
+//        }
+
+        //设置 Header 为 贝塞尔雷达 样式
+//        refreshLayout.setRefreshHeader(new BezierRadarHeader(getActivity()).setEnableHorizontalDrag(true));
+        //设置 Header 为 StoreHouse 样式
+//        refreshLayout.setRefreshHeader(new StoreHouseHeader(getActivity()).initWithPointList(getPointList()));
+        refreshLayout.setRefreshHeader(new WaveSwipeHeader(getActivity()));
+        refreshLayout.setPrimaryColors(222222);
+        refreshLayout.setEnableHeaderTranslationContent(false);
+        //设置 Footer 为 球脉冲 样式
+        refreshLayout.setRefreshFooter(new BallPulseFooter(getActivity()).setSpinnerStyle(SpinnerStyle.Scale));
+    }
+
+    @Override
     protected void initImmersionBar() {
         super.initImmersionBar();
         mImmersionBar.statusBarDarkFont(true, 0.2f)
                 .navigationBarColor(R.color.btn3)
                 .init();
     }
+
+    @Override
+    public void initData() {
+        super.initData();
+
+        refreshLayout.setOnRefreshListener(new OnRefreshListener() {
+            @Override
+            public void onRefresh(RefreshLayout refreshlayout) {
+                new Timer().schedule(new TimerTask() {
+                    @Override
+                    public void run() {
+                        //结束加载
+                        refreshLayout.finishRefresh();
+                        //加载失败的话3秒后结束加载
+                        refreshLayout.finishRefresh(3000);
+                    }
+                },3000);
+            }
+        });
+    }
+
+    private List<float[]> getPointList() {
+        // this point is taken from https://github.com/cloay/CRefreshLayout
+        List<Point> startPoints = new ArrayList<>();
+        startPoints.add(new Point(240, 80));
+        startPoints.add(new Point(270, 80));
+        startPoints.add(new Point(265, 103));
+        startPoints.add(new Point(255, 65));
+        startPoints.add(new Point(275, 80));
+        startPoints.add(new Point(275, 80));
+        startPoints.add(new Point(302, 80));
+        startPoints.add(new Point(275, 107));
+
+        startPoints.add(new Point(320, 70));
+        startPoints.add(new Point(313, 80));
+        startPoints.add(new Point(330, 63));
+        startPoints.add(new Point(315, 87));
+        startPoints.add(new Point(330, 80));
+        startPoints.add(new Point(315, 100));
+        startPoints.add(new Point(330, 90));
+        startPoints.add(new Point(315, 110));
+        startPoints.add(new Point(345, 65));
+        startPoints.add(new Point(357, 67));
+        startPoints.add(new Point(363, 103));
+
+        startPoints.add(new Point(375, 80));
+        startPoints.add(new Point(375, 80));
+        startPoints.add(new Point(425, 80));
+        startPoints.add(new Point(380, 95));
+        startPoints.add(new Point(400, 63));
+
+        List<Point> endPoints = new ArrayList<>();
+        endPoints.add(new Point(270, 80));
+        endPoints.add(new Point(270, 110));
+        endPoints.add(new Point(270, 110));
+        endPoints.add(new Point(250, 110));
+        endPoints.add(new Point(275, 107));
+        endPoints.add(new Point(302, 80));
+        endPoints.add(new Point(302, 107));
+        endPoints.add(new Point(302, 107));
+
+        endPoints.add(new Point(340, 70));
+        endPoints.add(new Point(360, 80));
+        endPoints.add(new Point(330, 80));
+        endPoints.add(new Point(340, 87));
+        endPoints.add(new Point(315, 100));
+        endPoints.add(new Point(345, 98));
+        endPoints.add(new Point(330, 120));
+        endPoints.add(new Point(345, 108));
+        endPoints.add(new Point(360, 120));
+        endPoints.add(new Point(363, 75));
+        endPoints.add(new Point(345, 117));
+
+        endPoints.add(new Point(380, 95));
+        endPoints.add(new Point(425, 80));
+        endPoints.add(new Point(420, 95));
+        endPoints.add(new Point(420, 95));
+        endPoints.add(new Point(400, 120));
+        List<float[]> list = new ArrayList<>();
+
+        int offsetX = Integer.MAX_VALUE;
+        int offsetY = Integer.MAX_VALUE;
+
+        for (int i = 0; i < startPoints.size(); i++) {
+            offsetX = Math.min(startPoints.get(i).x, offsetX);
+            offsetY = Math.min(startPoints.get(i).y, offsetY);
+        }
+        for (int i = 0; i < endPoints.size(); i++) {
+            float[] point = new float[4];
+            point[0] = startPoints.get(i).x - offsetX;
+            point[1] = startPoints.get(i).y - offsetY;
+            point[2] = endPoints.get(i).x - offsetX;
+            point[3] = endPoints.get(i).y - offsetY;
+            list.add(point);
+        }
+        return list;
+    }
+
 }
--- app/src/main/java/com/wanandroid/com/fragment/UserFragment.java
+++ app/src/main/java/com/wanandroid/com/fragment/UserFragment.java
@@ -1,15 +1,21 @@
 package com.wanandroid.com.fragment;
 
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.widget.ImageView;
 
+import com.scwang.smartrefresh.layout.api.RefreshHeader;
+import com.scwang.smartrefresh.layout.api.RefreshLayout;
+import com.scwang.smartrefresh.layout.listener.SimpleMultiPurposeListener;
 import com.wanandroid.com.R;
 import com.wanandroid.com.base.BaseFragment;
 import com.wanandroid.com.base.BasePresenter;
 
 import butterknife.Bind;
+import butterknife.ButterKnife;
 
 /**
  * author: fupengyu
@@ -17,10 +23,13 @@ import butterknife.Bind;
  */
 
 public class UserFragment extends BaseFragment {
-//    @Bind(R.id.toolbar)
-//    Toolbar toolbar;
-    @Bind(R.id.iv_user_header)
-    ImageView ivUserHeader;
+
+    @Bind(R.id.parallax)
+    ImageView parallax;
+    @Bind(R.id.toolbar)
+    Toolbar toolbar;
+    private int mOffset = 0;
+    private int mScrollY = 0;
 
     @Override
     protected BasePresenter createPresenter() {
@@ -42,6 +51,56 @@ public class UserFragment extends BaseFragment {
 //        ImmersionBar.setTitleBar(getActivity(), toolbar);
     }
 
+    @Override
+    public void initView(View rootView) {
+        super.initView(rootView);
+
+        final RefreshLayout refreshLayout = (RefreshLayout) rootView.findViewById(R.id.refreshLayout);
+        //设置 Header 为 贝塞尔雷达 样式
+//        refreshLayout.setRefreshHeader(new BezierRadarHeader(getActivity()).setEnableHorizontalDrag(true));
+        //设置 Footer 为 球脉冲 样式
+//        refreshLayout.setRefreshFooter(new BallPulseFooter(getActivity()).setSpinnerStyle(SpinnerStyle.Scale));
+        //设置 Header 为 经典 样式
+//        refreshLayout.setRefreshHeader(new ClassicsHeader(getActivity()));
+
+        refreshLayout.setOnMultiPurposeListener(new SimpleMultiPurposeListener() {
+            @Override
+            public void onRefresh(@NonNull RefreshLayout refreshLayout) {
+                refreshLayout.finishRefresh(3000);
+            }
+
+            @Override
+            public void onLoadMore(@NonNull RefreshLayout refreshLayout) {
+                refreshLayout.finishLoadMore(2000);
+            }
+
+            @Override
+            public void onHeaderMoving(RefreshHeader header, boolean isDragging, float percent, int offset, int headerHeight, int maxDragHeight) {
+                mOffset = offset / 2;
+                parallax.setTranslationY(mOffset - mScrollY);
+                toolbar.setAlpha(1 - Math.min(percent, 1));
+            }
+//            @Override
+//            public void onHeaderPulling(@NonNull RefreshHeader header, float percent, int offset, int bottomHeight, int maxDragHeight) {
+//                mOffset = offset / 2;
+//                parallax.setTranslationY(mOffset - mScrollY);
+//                toolbar.setAlpha(1 - Math.min(percent, 1));
+//            }
+//            @Override
+//            public void onHeaderReleasing(@NonNull RefreshHeader header, float percent, int offset, int bottomHeight, int maxDragHeight) {
+//                mOffset = offset / 2;
+//                parallax.setTranslationY(mOffset - mScrollY);
+//                toolbar.setAlpha(1 - Math.min(percent, 1));
+//            }
+        });
+
+    }
+
+    @Override
+    public void initData() {
+        super.initData();
+    }
+
 //    @Override
 //    protected void initImmersionBar() {
 //        super.initImmersionBar();
@@ -49,4 +108,10 @@ public class UserFragment extends BaseFragment {
 //                .navigationBarColor(R.color.btn3)
 //                .init();
 //    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        ButterKnife.unbind(this);
+    }
 }
